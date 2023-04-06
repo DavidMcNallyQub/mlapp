7ae9ceff59cf03e0b144c1f43cb34b76296a65bf
David McNally <dmcnally16@qub.ac.uk>
2023-04-06 20:26:52 +0100

    Update schema.sql for users to have email addresses and issue table instead of posts. Add docstring to auth.py and db.py. Some small changes to register and base_layout html templates.

--- mlapp/auth.py
+++ mlapp/auth.py
@@ -1,23 +1,39 @@
 import functools
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, Response
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from mlapp.db import get_db
 
+from typing import Union
+
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+
 @bp.route('/register', methods=('GET', 'POST'))
-def register():
+def register() -> Union[Response, str]:
+    """The register view function.           
+
+    Process a request to the register URL.
+    For a POST request retrieve the user's email and password and register the user. 
+    After registration, redirect the user to the login page. 
+    If an email or a password is not provided, or the user is already registered in the database, 
+    render the register page and Flash corresponding error. 
+    For a GET request, render the register page.
+
+    Returns:
+        Union[Response, str]: For a successful POST request return a Redirect Reponse. 
+        For a GET request or a error on a POST request, return a Rendered template string.
+    """
     if request.method == 'POST':
-        username = request.form['email']
+        email = request.form['email']
         password = request.form['password']
         db = get_db()
         error = None
 
-        if not username:
+        if not email:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
@@ -25,12 +41,12 @@ def register():
         if error is None:
             try:
                 db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password)),
+                    "INSERT INTO user (email, password) VALUES (?, ?)",
+                    (email, generate_password_hash(password)),
                 )
                 db.commit()
             except db.IntegrityError:
-                error = f"User {username} is already registered."
+                error = f"User {email} is already registered."
             else:
                 return redirect(url_for("auth.login"))
 
@@ -38,19 +54,33 @@ def register():
 
     return render_template('auth/register.html')
 
+
 @bp.route('/login', methods=('GET', 'POST'))
-def login():
+def login() -> Union[Response, str]:
+    """The login view function.
+
+    Process a request to the login URL.
+    For a POST request, retrieve the user's email and password and log the user in.
+    After log-in, redirect the user to the homepage. 
+    If the user's email does not exist in the database or their password does not match,
+    render the login page and Flash any error.
+    For a GET request, render the login page. 
+
+    Returns:
+        Union[Response, str]: For a successful POST request return a Redirect Reponse. 
+        For a GET request or a error on a POST request, return a Rendered template string.
+    """
     if request.method == 'POST':
-        username = request.form['email']
+        email = request.form['email']
         password = request.form['password']
         db = get_db()
         error = None
         user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
+            'SELECT * FROM user WHERE email = ?', (email,)
         ).fetchone()
 
         if user is None:
-            error = 'Incorrect username.'
+            error = 'Incorrect email.'
         elif not check_password_hash(user['password'], password):
             error = 'Incorrect password.'
 
@@ -63,8 +93,16 @@ def login():
 
     return render_template('auth/login.html')
 
+
 @bp.before_app_request
 def load_logged_in_user():
+    """Check if a user_id is stored in the session, gets that users data from the database 
+    and store it on g.user. 
+
+    For any requested URL load_logged_in_user will be run. This allows a logged in user's
+    information to be made available to other view functions. If there is no user_id, g.user
+    will be None. 
+    """
     user_id = session.get('user_id')
 
     if user_id is None:
@@ -74,12 +112,33 @@ def load_logged_in_user():
             'SELECT * FROM user WHERE id = ?', (user_id,)
         ).fetchone()
 
+
 @bp.route('/logout')
-def logout():
+def logout() -> Response:
+    """Remove the current user_id from the session. 
+
+    A user's will not be loaded on subsequent requests (See load_logged_in_user()). 
+
+    Returns:
+        Response: _description_
+    """
     session.clear()
     return redirect(url_for('index'))
 
-def login_required(view):
+
+def login_required(view) -> Union[Response, str]:
+    """A decorator that wraps the origional view it is applied to and returns an new view function.
+
+    The new view function checks if a user is logged in. 
+    If a user is not logged in, the user is directed to the login page. 
+    Otherwise, the original view function is returned and continues normally.
+    
+    Args:
+        view (_type_): _description_
+
+    Returns:
+        Union[Response, str]: _description_
+    """
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
--- mlapp/db.py
+++ mlapp/db.py
@@ -1,27 +1,47 @@
 import sqlite3
-
+from sqlite3 import Connection
 import click
 from flask import current_app, g
 
+def get_db() -> Connection:
+    """Return the SQLite3 database connection.
 
-def get_db():
+    The connection to the SQLite database is stored in the g object. g is unique for 
+    all requests and can store data that can be accessed by multiple functions during requests.
+    get_db() will be called when the application has been created and is handling a request, 
+    so current_app() can be used. 
+    sqlite3.connect() establishes a connection to the file pointed at by the DATABASE
+    configuration key.
+    Columns can be accessed by name as sqlite3.Row tells the connection to return rows that
+    act similarly to dictionaries.
+
+    Returns:
+        Connection: A SQLite3 Connection object.
+    """
     if 'db' not in g:
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
-
     return g.db
 
-
 def close_db(e=None):
-    db = g.pop('db', None)
+    """Close a database connection if it exists.
 
+    Checks if g.db was set, if so, db is removed from the g object and closed() is called
+    on db.
+
+    Args:
+        e (_type_, optional): _description_. Defaults to None.
+    """
+    db = g.pop('db', None)
     if db is not None:
         db.close()
 
 def init_db():
+    """Initialise the SQLite3 database.
+    """
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
@@ -30,10 +50,20 @@ def init_db():
 
 @click.command('init-db')
 def init_db_command():
-    """Clear the existing data and create new tables."""
+    """Clear the existing data and create new tables.
+    """
     init_db()
     click.echo('Initialized the database.')
 
 def init_app(app):
+    """Register the close_db() and init_db_command() with the application instance. 
+
+
+    close_db() will be called after a response and init_db_command() will add a new command
+    that can be called with the 'flask' command
+
+    Args:
+        app (Flask): A Flask application instance.
+    """
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
