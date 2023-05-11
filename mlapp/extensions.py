570ab517c4e03a74c871266e6e9a5666b431481d
David McNally <dmcnally16@qub.ac.uk>
2023-05-11 17:08:43 +0100

    Removed extensions.py as this branch doesn't use SQLAlchemy

--- mlapp/extensions.py
+++
@@ -1,6 +0,0 @@
-"""
-A module that contains extensions to the Flask application.
-"""
-from flask_sqlalchemy import SQLAlchemy
-# create SQLAlchemy database abstraction layer extension
-dbase = SQLAlchemy()
